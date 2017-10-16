0ccfde1dd4fae965343580d9830af72bf9095bb9
kukkar <sahil.kukkar99@gmail.com>
2017-10-16 17:01:30 +0530

    [SK] fianl

---
+++ clientsocket.js
@@ -0,0 +1,19 @@
+var io = require('socket.io-client');
+var socket = io('http://localhost:3000',{ forceNew: true });
+
+
+
+socket.on('connect', () => {
+  console.log(socket.id); // 'G5p5...'
+  socket.emit('newuser',socket.id);
+
+});
+socket.on('rival',(pairid) => {
+	console.log(socket.id);
+	console.log("my rival is ",pairid);
+});
+
+
+
+
+
